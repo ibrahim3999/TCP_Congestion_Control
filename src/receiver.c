05134cad5ab1736af5288b957691c604932dddfc
ibrahim3999 <mgdlhb@gmail.com>
2022-12-11 21:45:44 +0200

    Receiver Accpet

---
+++ src/receiver.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#define PORT 9999
+void main()
+{
+    int sockrec ,connrec,len;
+    struct sockaddr_in recaddr,cli;
+
+    sockrec=socket(AF_INET,SOCK_STREAM,0);
+    if(sockrec==-1)
+    {
+        printf("socket creation failed ..\n");
+    }
+    else{
+        printf("Socket Successfully created ..\n");
+    }
+
+    bzero(&recaddr,sizeof(recaddr));
+
+    recaddr.sin_family=AF_INET;
+    recaddr.sin_addr.s_addr=inet_addr("127.0.0.1");
+    recaddr.sin_port=htons(PORT);
+    if((bind(sockrec,(struct sockaddr*)&recaddr,sizeof(recaddr)))!=0)
+    {
+        printf("socket bind failed ... !!!\n");
+        exit(0);
+    }
+    else{
+        printf("Sokect successfully binded ...!!!\n");
+    }
+    if((listen (sockrec,5))!=0)
+    {
+        printf("Listen failed ...\n");
+        exit(0);
+    }
+    else
+    printf("Receiver listening ... \n");
+
+    len=sizeof(cli);
+
+    connrec=accpet(sockrec,(struct sockaddr*)&cli,&len);
+    if(connrec<0)
+    {
+        printf("Receiver accept failed ...\n");
+        exit(0);
+    }
+    else
+        printf("Receiver accept the Sender ... \n ");
+
+
+}
