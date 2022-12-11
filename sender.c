d52c573564793af119057d1d66b832b7495c4153
ibrahim3999 <mgdlhb@gmail.com>
2022-12-11 17:03:39 +0200

    ReaderFile()

--- sender.c
+++ sender.c
@@ -1,1 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void ReaderFile()
+{
+
+    FILE *ptr;
+    char ch;
+    ptr=fopen("file.txt","r");
+
+    if(ptr==NULL)
+        printf("file can not be opened");
+
+    while (ch!=EOF)
+    {
+        ch=fgetc(ptr);
+        printf("%c\n",ch);
+    }
+    fclose(ptr);
+}
+void main()
+{
+    ReaderFile();
+
+
+}
