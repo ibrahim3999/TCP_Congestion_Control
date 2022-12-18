c3f8412c6b2c88d1dfb0d3b1df6148b6b785d55d
ibrahim3999 <mgdlhb@gmail.com>
2022-12-18 22:44:23 +0200

    update

--- src/receiver.c
+++ src/receiver.c
@@ -1,22 +1,21 @@
 #include <stdio.h>
 #include <sys/time.h>
-#include <sys/types.h>
 #include <stdlib.h>
 #include <errno.h>
 #include <strings.h>
 #include <string.h>
-#include <unistd.h>
-#include <signal.h>
-#include <time.h>
+#include <sys/types.h>
 #include <netinet/in.h>
 #include <netinet/tcp.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
+#include <unistd.h>
+#include <signal.h>
+#include <time.h>
 
 
-
-
-
+#define BUFF_SIZE 64000
+#define MILLION  100000000.0
 #define SERVER_PORT 5566
 #define SERVER_IP_ADDRESS "127.0.0.1"
 #define SIZE 1048576
@@ -28,178 +27,264 @@
 #define XOR "0001 1010 1101 1010"
 #define SA struct sockaddr
 #define SI struct sockaddr_in
+int setSock();
+
+void printTimes(double *timePart1, double *timePart2, int len,double sum_Cubic,double sum_Reno);
 
 int main() {
-    //allocate memory for authentication and parts of content's file
-    char buffer[HALF_SIZE];
-    char buffercheck[AUTH_SIZE];
-    //output of xor - authentication
-    strcpy(buffercheck,"0101 0001 1100 0000");
 
-    int size = 0;
+
     int size2=0;
-    char exitmessage[1];
-    
-    FILE *file_pointer;
-    socklen_t length;
-    int count_us=0;
+    // on linux to prevent crash on closing socket.
+    signal(SIGPIPE, SIG_IGN);
+    char buffer[HALF_SIZE];
+    char buffreExit[1];
+    char buffer_auth[AUTH_SIZE];
+    struct timeval start, end;
+    int counter = 0;
+
+    int counter_avg = 0;
+
+    //Accept and incoming connection
+    printf("[+]TCP protocol socket created -> Waiting for connection with sender\n");
+
+    struct sockaddr_in client_addr;
+    socklen_t client_addr_length = sizeof(client_addr);
+
+    double sum_for_average_cubic = 0.0;
+    double sum_for_average_reno = 0.0;
+    double *part1 = NULL;
+    double *part2 = NULL;
+    part1 = (double *) malloc(1);
+    part2 = (double *) malloc(1);
     char namealgo[250];
-    int flag=1;
+    socklen_t length;
 
-    struct timeval start1,end1;
+    //Check allocate
 
-    int client_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if(client_socket == -1) {
-        fprintf(stderr, "Socket ERROR - could not create the socket : %s\n", strerror(errno));
-        exit(EXIT_FAILURE); 
+    char messageexit[1] ;
+    messageexit[0]= '0';
+    int sockfd = -1;
+    int countbytes = 0;
+    int j=1;
+    sockfd = setSock(); //Create the socket
+    int checkPart = 0;
+    int rcv=0;
+    int Bole=1;
+
+    do {
+        
+
+        countbytes = 0;
+
+        memset(&client_addr, 0, sizeof(client_addr));
+        client_addr_length = sizeof(client_addr);
+
+        //The receiver accept the connection
+        int client_socket = accept(sockfd, (SA *) &client_addr, &client_addr_length);
+        if (client_socket == -1) {
+            printf("listen failed with error code : \n");
+            //close(sockfd);
+            return -1;
+        } else {
+            printf("[+]client number connection accepted\n");
+        }
+        
+
+        while (1) {
+            printf("incheck6\n");
+
+            gettimeofday(&start, 0);
+            rcv = recv(client_socket, &buffer, HALF_SIZE, 0);
+            printf("1\t%d\n",rcv);
+            countbytes += rcv;
+            printf("2\t%d\n",countbytes);
+
+            if (countbytes == HALF_SIZE) {
+
+
+                int k = send(client_socket,XOR, sizeof(buffer_auth),0);
+                printf("[+]Sent Ack. Authentication!\n");
+                gettimeofday(&end, 0);
+                //printf("%d\n",countbytes);
+                if (checkPart == 0) { //To handle with  the first recv part
+
+                    printf("Checkpart1\n");
+
+                    double time_spent = (end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec) / MILLION;
+
+
+                    sum_for_average_cubic += time_spent;
+                    *(part1 + counter++) = time_spent;
+                    printf("Time in microseconds: %f microseconds\n", time_spent);
+                    checkPart = 1;
+                    size2+=countbytes;
+                    countbytes=0;
+
+
+
+                } else { // To handle with  the second recv part
+
+                    printf("Checkpart2\n");
+
+                    bzero(namealgo, sizeof(namealgo));
+                    printf("\nSwitching to Reno algorithm\n");
+                    strcpy(namealgo, "reno");
+                    length = strlen(namealgo);
+                    if ((setsockopt(client_socket, IPPROTO_TCP, TCP_CONGESTION, namealgo, length)) != 0) {
+
+                        printf("setsockopt_receiver\n");
+
+                        return -1;
+
+                    }
+                    double time_spent = (end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec) / MILLION;
+
+                    sum_for_average_cubic += time_spent;
+                    *(part2 + counter++) = time_spent;
+                    printf("Time in microseconds: %f microseconds\n", time_spent);
+                    checkPart = 0;
+                    size2+=countbytes;
+                    countbytes=0;
+                }
+                part1 = (double *) realloc(part1, sizeof(double) * (counter + 2));
+                part2 = (double *) realloc(part2, sizeof(double) * (counter + 2));
+
+
+                printf("\nsize2==%d\n\n",size2);
+                if (size2 == SIZE) {
+
+                    printf("All the file is recv done\n");
+                    printf("\n****\n");
+                    // To get the decision of the user
+                    bzero(buffreExit, sizeof(buffreExit));
+                    printf("\n\n");
+                    int x = recv(client_socket, &buffreExit, sizeof(buffreExit), 0);
+                    printf("%c\n",buffreExit[0]);
+                    printf("%d\n", x);
+                    if (buffreExit[0] == messageexit[0]) {
+
+//                  printTimes(part1, part2, counter, sum_for_average_cubic, sum_for_average_reno);
+                        //close(client_socket);
+                    Bole =0;
+                        printf("inCheck1!!\n");
+
+                    break;
+                    }else{
+                        Bole =1;
+                        printf("inCheck2!!\n");
+                        size2=0;
+                    }
+
+
+
+
+                }
+
+            }
+
+//            if (size2==SIZE){
+//                printf("inCheck3!!\n");
+//
+//                break;
+//            }
+            //printf("*\n");
+
+        }
+        printf("inCheck4!!\n");
+        close(client_socket);
+        //size2=0;
+
+        counter_avg++;
+
+    }while(Bole);
+
+    //printTimes(part1, part2, counter, sum_for_average_cubic, sum_for_average_reno);
+
+    close(sockfd);
+    return 0;
+}
+
+
+
+
+
+
+void printTimes(double *timePart1, double *timePart2, int len,double sum_Cubic,double sum_Reno) {
+
+
+
+    printf("Times Summary:\n");
+    printf("Part1:-\n");
+    for (int j = 0; j < len; j++) {
+        printf("%f\n", *(timePart1 + j));
     }
-    else printf("[+]TCP protocol socket created.\n");
 
-    //Sets the first count bytes of destination
-    struct sockaddr_in server_address;
-    memset(&server_address, 0, sizeof(server_address));
-    server_address.sin_family = AF_INET;
-    server_address.sin_port = htons(SERVER_PORT);
-    int rval = inet_pton(AF_INET, (const char*)SERVER_IP_ADDRESS, &server_address.sin_addr);
-    if(rval <= 0) {
-        printf("Failed to convert an IP to Binary format!");
+    printf("Part2:-\n");
+    for (int j = 0; j < len; j++) {
+        printf("%f\n", *(timePart2 + j));
+    }
+
+    double avg_cubic = sum_Cubic / len;
+    double avg_reno = sum_Reno / len;
+
+    printf("\n******\n");
+
+    printf("Averages:\n");
+    printf("Part1:\t%f\n", avg_cubic);
+
+    printf("Averages:\n");
+    printf("Part2:\t%f\n", avg_reno);
+
+    printf("\nEND\n");
+
+    free(timePart1);
+    free(timePart2);
+
+
+}
+
+int setSock(){
+
+
+    SI server_addr;
+    char namealgo[250];
+    socklen_t length;
+
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(SERVER_PORT); // short, network byte order
+
+    // create a socket lisener.
+    int sockfd = -1;
+    if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        printf("Couldn't create a socket listener : %d\n",errno);
+        perror("socket\n");
         return -1;
     }
 
-    // Connect to the receiver
-    int connection = connect(client_socket, (struct sockaddr *) &server_address, sizeof(server_address));
-    if(connection == -1) {
-        fprintf(stderr, "The connecting to receiver failed - error msg code:%s\n", strerror(errno));
-        exit(EXIT_FAILURE); 
-    }
-    else {
-        printf("[+]Sender connected to the Receiver successfully!!\n");
+    strcpy(namealgo,"cubic");
+    length = sizeof(namealgo);
+    if (getsockopt(sockfd,IPPROTO_TCP,TCP_CONGESTION,namealgo,&length)!=0){
+
+        perror("getsockopt\n");
+        return -1;
     }
 
-    
-
-    while(flag) {
-
-        int j = 0;
-        count_us++;
-        
-        if (count_us>1)
-        {
-            printf("Changing the CC algorithm to the default ->cubic\n");
-        }
-        
-
-            // Create clinet socket
-
-            // By default cc algorithm is cubic
-            strcpy(namealgo,"cubic");
-            int get_sock_opt = getsockopt(client_socket, IPPROTO_TCP, TCP_CONGESTION, namealgo, &length);
-            if( get_sock_opt != 0) {
-                perror("getsockopt");
-                exit(EXIT_FAILURE);
-            }
-
-            file_pointer = fopen(FIlE_NAME, "r");
-            if(file_pointer == NULL) {
-                fprintf(stderr, "Failed open the file f1.txt : %s\n", strerror(errno));
-                exit(EXIT_FAILURE);
-            }
-
-                //sending the part 1 of the content while FILE OPEN
-                printf("[+]send the first part of the file\n");
-
-                fread(buffer,1, sizeof(buffer),file_pointer);
-                size = send(client_socket, buffer, sizeof(buffer), 0);
-                if(size<0){
-                    printf("ERROR - Send the half 1 of f1.txt-> failed with error!\n");
-                    exit(1);
-                }
-                
-            //the size of half file!
-            printf("%d of content sent!\n",size);
-            size2+=size;
-
-        if (size==HALF_SIZE) {
-            printf("\n");
-            int x=recv(client_socket, &buffercheck, sizeof(buffercheck), 0);
-            
-            
-            
-        
-            if (strcmp(XOR, buffercheck) == 0) {
-
-                printf("Authentication success!!\n");
-
-            }
-
-        }
-            //changing the CC Algorithem by setsockopt
-            printf("[+]Change the cc algorithm to reno\n");
-            strcpy(namealgo, "reno");
-            length = sizeof(namealgo);
-            int set_sock_opt = setsockopt(client_socket, IPPROTO_TCP, TCP_CONGESTION, namealgo, length);
-            if (set_sock_opt != 0) {
-                perror("setsockopt");
-                exit(EXIT_FAILURE);
-            }
-
-            
-
-
-            bzero(buffer, sizeof(buffer));
-            printf("\n [+]Send the second part of the file\n\n");
-            size = 0;
-            fread(buffer, 1, sizeof(buffer), file_pointer);
-            size = send(client_socket, buffer, sizeof(buffer), 0);
-            if (size < 0) {
-                printf("Send part2 failed with error!\n");
-                exit(1);
-            }
-
-
-            size2 += size;
-            printf("%d of content sent!\n", size);
-            if (size2 == SIZE) {
-                printf("sent all the 2MB file: %d\n", size2);
-                fclose(file_pointer);
-                printf("User Decision:\n ");
-                printf("Insert on 1 to send file again OR 0 to exit the program!\n");
-                int flag2;
-                scanf("%d", &flag2);
-                flag = flag2;
-
-                printf("*****\n");
-
-
-                if (flag2 == 0) {
-
-                    exitmessage[0] = '0';
-                    printf("\nSender send the exit message for the receiver!!\n");
-                }
-                else{
-                        exitmessage[0] = '1';
-
-                }
-
-
-                    
-                    int x= send(client_socket, exitmessage, sizeof(exitmessage), 0);
-                    printf("%d\n",x);
-                    //fclose(file_pointer);
-
-
-            } else {
-                printf("sent just %d out of %d\n", size2, SIZE);
-
-            }
-
-
-
-            j++;
-            size = 0;
-            size2 = 0;
+    // connect the server to a port which can read and write on.
+    if(bind(sockfd, (SA *)&server_addr, sizeof(server_addr)) == -1) {
+        printf("Bind failed with error code : %d" , errno);
+        return -1;
     }
-    close(client_socket);
 
-    return 0;
+    printf("Receiver connected to the Sender successfully!\n\n");
+
+
+    if(listen(sockfd, 500) == -1) {
+        printf("listen() failed with error code : %d",errno);
+        return -1;
+    }
+
+    return sockfd;
 }
--- src/sender.c
+++ src/sender.c
@@ -16,14 +16,14 @@
 
 
 
-#define AUTH_SIZE 21
-#define FIlE_NAME "f2.txt"
+
 #define SERVER_PORT 5566
 #define SERVER_IP_ADDRESS "127.0.0.1"
-#define SIZE 1048576  
+#define SIZE 1048576
 #define HALF_SIZE 524288
-
-// ID AUTHORS: 313357196, 207101734
+#define AUTH_SIZE 21
+#define FIlE_NAME "f2.txt"
+// 313357196, 207101734
 
 #define XOR "0001 1010 1101 1010"
 #define SA struct sockaddr
@@ -33,24 +33,25 @@ int main() {
     //allocate memory for authentication and parts of content's file
     char buffer[HALF_SIZE];
     char buffercheck[AUTH_SIZE];
-    //authentication
+    //output of xor - authentication
     strcpy(buffercheck,"0101 0001 1100 0000");
 
     int size = 0;
-    int send_size=0;
+    int size2=0;
     char exitmessage[1];
     
     FILE *file_pointer;
     socklen_t length;
     int count_us=0;
     char namealgo[250];
-    
+    int flag=1;
+
     struct timeval start1,end1;
 
     int client_socket = socket(AF_INET, SOCK_STREAM, 0);
     if(client_socket == -1) {
-        perror("[-]Error created socket");
-        exit(1); 
+        fprintf(stderr, "Socket ERROR - could not create the socket : %s\n", strerror(errno));
+        exit(EXIT_FAILURE); 
     }
     else printf("[+]TCP protocol socket created.\n");
 
@@ -68,17 +69,18 @@ int main() {
     // Connect to the receiver
     int connection = connect(client_socket, (struct sockaddr *) &server_address, sizeof(server_address));
     if(connection == -1) {
-        perror("[-]The connecting to receiver failed\n");
-        exit(1); 
+        fprintf(stderr, "The connecting to receiver failed - error msg code:%s\n", strerror(errno));
+        exit(EXIT_FAILURE); 
     }
+    else {
+        printf("[+]Sender connected to the Receiver successfully!!\n");
+    }
+
     
-    printf("[+]Sender connected to the Receiver successfully!!\n");
 
-    char flag='m';
+    while(flag) {
 
-    while(flag!='e') {
-
-        
+        int j = 0;
         count_us++;
         
         if (count_us>1)
@@ -96,88 +98,109 @@ int main() {
                 perror("getsockopt");
                 exit(EXIT_FAILURE);
             }
-            //open my file
+
             file_pointer = fopen(FIlE_NAME, "r");
             if(file_pointer == NULL) {
                 fprintf(stderr, "Failed open the file f1.txt : %s\n", strerror(errno));
                 exit(EXIT_FAILURE);
             }
 
-                //part one sending
+                //sending the part 1 of the content while FILE OPEN
                 printf("[+]send the first part of the file\n");
 
                 fread(buffer,1, sizeof(buffer),file_pointer);
                 size = send(client_socket, buffer, sizeof(buffer), 0);
                 if(size<0){
-                    perror("[+]ERROR - Send the half 1 of f1.txt-> failed with error!\n");
+                    printf("ERROR - Send the half 1 of f1.txt-> failed with error!\n");
                     exit(1);
                 }
-            // Half size 
-
-            send_size+=size;
+                
+            //the size of half file!
+            printf("%d of content sent!\n",size);
+            size2+=size;
 
         if (size==HALF_SIZE) {
             printf("\n");
             int x=recv(client_socket, &buffercheck, sizeof(buffercheck), 0);
+            
+            
+            
+        
+            if (strcmp(XOR, buffercheck) == 0) {
 
-            if (strcmp(XOR, buffercheck) == 0) {printf("[+]Authentication success!!\n");}
+                printf("Authentication success!!\n");
+
+            }
 
         }
-            //Change algorithm 
+            //changing the CC Algorithem by setsockopt
             printf("[+]Change the cc algorithm to reno\n");
             strcpy(namealgo, "reno");
             length = sizeof(namealgo);
             int set_sock_opt = setsockopt(client_socket, IPPROTO_TCP, TCP_CONGESTION, namealgo, length);
             if (set_sock_opt != 0) {
                 perror("setsockopt");
-                exit(1);
+                exit(EXIT_FAILURE);
             }
 
             
 
-            //sending the next half file to receiver
+
             bzero(buffer, sizeof(buffer));
             printf("\n[+]Send the second part of the file\n\n");
             size = 0;
             fread(buffer, 1, sizeof(buffer), file_pointer);
             size = send(client_socket, buffer, sizeof(buffer), 0);
             if (size < 0) {
-                printf("[-]Falied sender second PART !!\n");
+                printf("Send part2 failed with error!\n");
                 exit(1);
             }
 
 
-            send_size += size;
-            if (send_size == SIZE) {
-                printf("[+]sent all the 2MB file: %d\n", send_size);
+            size2 += size;
+            printf("%d of content sent!\n", size);
+            if (size2 == SIZE) {
+                printf("[+]sent all the 2MB file: %d\n", size2);
                 fclose(file_pointer);
                 printf("\nUser Decision:\n");
-                printf("Press Any char to send file again OR 'e' to exit the program!\n");
-                char flag2;
-                scanf("%c", &flag2);
+                printf("Press 1 to send file again OR 0 to exit the program!\n");
+                int flag2;
+                scanf("%d", &flag2);
                 flag = flag2;
 
-                printf("\n\n");
+                printf("##############################\n");
 
-                if (flag2 == 'e') {
 
-                    exitmessage[0] = 'e';
-                    printf("\n The client EXIT !!\n");
+                if (flag2 == 0) {
+
+                    exitmessage[0] = '0';
+                    printf("\nSender sent the exit message for the receiver!!\n");
                 }
-                int x= send(client_socket, exitmessage, sizeof(exitmessage), 0);
+                else{
+                        exitmessage[0] = '1';
+                        
+
+                }
+
+
                     
-                
+                    int x= send(client_socket, exitmessage, sizeof(exitmessage), 0);
+                    
+                    //fclose(file_pointer);
 
 
             } else {
-                printf("sent just %d out of %d\n", send_size, SIZE);
+                printf("sent just %d out of %d\n", size2, SIZE);
 
             }
 
+
+
+            j++;
             size = 0;
-            send_size = 0;
+            size2 = 0;
     }
     close(client_socket);
-    return 1;
 
+    return 0;
 }
